e7346a77d9279582ee267387ecec90ae282a3927
Anuj Panchal <anujpan1@gmail.com>
2018-10-26 19:35:11 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,4 +34,13 @@ class Song
         song.name == name
     end
   end
+
+  def self.find_or_create_by_name(name)
+    self.find_by_name(name) || self.create_by_name(name)
+  end
+   def self.alphabetical
+    self.all.sort_by do |song|
+        song.name
+    end
+  end
 end
