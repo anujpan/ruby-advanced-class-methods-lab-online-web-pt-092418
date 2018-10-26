2d17df073957aa2628721502495c30c395ac3806
Anuj Panchal <anujpan1@gmail.com>
2018-10-26 19:35:18 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -38,7 +38,8 @@ class Song
   def self.find_or_create_by_name(name)
     self.find_by_name(name) || self.create_by_name(name)
   end
-   def self.alphabetical
+
+  def self.alphabetical
     self.all.sort_by do |song|
         song.name
     end
